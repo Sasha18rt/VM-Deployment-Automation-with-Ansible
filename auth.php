929e115bfe503274b85948f371fb0fc3f49a62c7
Rotaienko Oleksandr <oleksandr.rotaienko@mif.stud.vu.lt>
2023-11-30 02:19:52 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,36 +1,32 @@
 <?php
 $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
-
-$pass = md5($pass . "cfifhjnf'yrj123");
-$mysqli = new mysqli('localhost', 'root', '', 'remote-med');
-$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
-$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+$role = filter_var(trim($_POST['role']), FILTER_SANITIZE_STRING);
 
 $pass = md5($pass . "cfifhjnf'yrj123");
 $mysqli = new mysqli('localhost', 'root', '', 'remote-med');
 
-// Assuming you meant to use $result instead of $user here
-$result = $mysqli->query("SELECT * FROM `patient` WHERE `Username` = '$login' AND `Password`= '$pass'");
+$tableName = ($role === 'doctor') ? 'doctor' : 'patient';
 
-// Check if there are no results for patients, then check for doctors
-if ($result === null || $result->num_rows == 0) {
-    $result = $mysqli->query("SELECT * FROM `doctor` WHERE `Login` = '$login' AND `Password`= '$pass'");
-}
+// Use a generic placeholder for the username column
+$usernameColumn = ($role === 'doctor') ? 'Login' : 'Username';
 
-// Assuming you meant to use $result instead of $user here
-$result = $mysqli->query("SELECT * FROM `patient` WHERE `Username` = '$login' AND `Password`= '$pass'");
-
-// Check if there are no results for patients, then check for doctors
-if ($result === null || $result->num_rows == 0) {
-    $result = $mysqli->query("SELECT * FROM `doctor` WHERE `Login` = '$login' AND `Password`= '$pass'");
-}
+$result = $mysqli->query("SELECT * FROM `$tableName` WHERE `$usernameColumn` = '$login' AND `Password`= '$pass'");
 
 $user = $result->fetch_assoc();
 if ($user === null || count($user) == 0) {
-    echo "wrong login or password";
+    echo "Wrong login or password";
     exit();
 }
+
+setcookie('user', $user['Name'], time() + 3600 * 24, "/");
+setcookie('p_id', $user['p_id'], time() + 3600 * 24, "/");
+setcookie('role', $_POST['role'], time() + 3600 * 24, "/");
+
+$mysqli->close();
+header('Location: /');
+?>
+
 
 setcookie('user', $user['Name'], time() + 3600 * 24, "/");
 setcookie('p_id', $user['p_id'], time() + 3600 * 24, "/");
