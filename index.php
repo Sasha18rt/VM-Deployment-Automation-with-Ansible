dc0cffab858893c5dd15837214bd7c73264460a7
Rotaienko Oleksandr <oleksandr.rotaienko@mif.stud.vu.lt>
2023-11-30 02:22:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,47 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css"> 
-    <title>Sign Up</title>
+    <title>Simple Website</title>
 </head>
 <body>
     <div class="container">
-        <?php
-          if(!isset($_COOKIE['user']) || $_COOKIE['user'] == ''):
-        ?>
-       <div class="row"> 
-        <div class="col">
-            <h1>Registration</h1>
-            <form action="check.php" method="post">
-                <div class="mb-3">
-                    <input type="text" class="form-control" name="login" id="login" placeholder="Input Login">
+        <?php if (!isset($_COOKIE['user']) || $_COOKIE['user'] == ''):?>
+            <div class="row d-flex align-items-center justify-content-center" style="min-height: 100vh;"> 
+                <div class="col-10 mx-auto registration-container">
+                    <h1>Login</h1>
+                    <form action="auth.php" method="post">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name="login" id="login" placeholder="Input Login">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" name="pass" id="pass" placeholder="Input Password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="role">Select Role:</label>
+                            <select class="form-control" name="role" id="role">
+                                <option value="patient">Patient</option>
+                                <option value="doctor">Doctor</option>
+                            </select>
+                        </div>
+                        <button class="btn btn-success" type="submit">Sign in</button> 
+                        <p class="text-muted">You don't have an account? <a href="/role.php">Sign up here</a>.</p>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <input type="text" class="form-control" name="name" id="name" placeholder="Input Name">
-                </div>
-                <div class="mb-3">
-                    <input type="password" class="form-control" name="pass" id="pass" placeholder="Input Password">
-                </div>
-                <button class="btn btn-success" type="submit">Sign Up</button>
-            </form>
-        </div>
-        <div class="col">
-            <h1>Login</h1>
-            <form action="auth.php" method="post">
-                <div class="mb-3">
-                    <input type="text" class="form-control" name="login" id="login" placeholder="Input Login">
-                </div>
-                <div class="mb-3">
-                    <input type="password" class="form-control" name="pass" id="pass" placeholder="Input Password">
-                </div>
-                <button class="btn btn-success" type="submit">Sign in</button> 
-                <p style="display: inline-block; margin-left: 10px;">You don't have an account? <a href="/role.php">Sign up here</a>.</p>
+            </div>
+        <?php else: ?>
+            <div class="container-fluid">
+            <header>
+                <h1>HOSPITAL</h1>
+            </header>
+
+            <nav>
+                <a href="/MyAccount.php">My Account</a>
+                <a href="/doctorsList.php">Make an appointment</a>
+                <a href="/exit.php">Logout</a>
+              
+            </nav>
+
+            <section id="my-account">
+           
+            <h2><?php echo ucfirst($_COOKIE['role']); ?> Account</h2>
+             <p>Hello <?php echo $_COOKIE['user']; ?>!</p>
+            </section>
+            
+
+
+            <section id="schedule-meeting">
+                <h2>About us</h2>
+                <p>some info.</p>
                 
-            </form>
+            </section>
         </div>
-       </div>
-       <?php else: ?>
-        <p> Hello <?= $_COOKIE['user'] ?>. To make an appointment, press <a href="/doctorsList.php/">here</a>. To log out, press <a href="/exit.php/">here</a>.</p>
-       <?php endif; ?>
-    </div>
+        <?php endif; ?>
+        </div>
 </body>
 </html>
