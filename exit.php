fc68c797099a99a61edac4243c70236739245b52
Rotaienko Oleksandr <oleksandr.rotaienko@mif.stud.vu.lt>
2023-12-12 22:19:05 +0200

    Update exit.php

--- exit.php
+++ exit.php
@@ -3,6 +3,6 @@ setcookie( 'user',$user['name'], time() - 3600 * 24, "/" );
 setcookie('p_id', $user['p_id'], time() - 3600 * 24, "/");
 setcookie('d_id', $user['d_id'], time() - 3600 * 24, "/");
 setcookie('role', $_POST['role'], time() - 3600 * 24, "/");
-header('Location: /')
+header('Location: /web/')
 
 ?>
